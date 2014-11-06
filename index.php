5ab06de78773161f0e2536196d8d8cccbc1b7502
slayerfat <slayerfat@gmail.com>
2014-11-06 23:43:54 +0000

    modificado index.php y borrado archivos inecesarios

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pre index del asunto</title>
+</head>
+<body>
+  <div>
+    <p>
+      index: <a href="rev2/index.php">version rev2</a>
+    </p>
+    <p>
+      <a href="mysql.php">mysql</a>
+    </p>
+  </div>
+</body>
+</html>
