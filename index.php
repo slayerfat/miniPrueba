d8bec578f2d65238988bbd49632cb2467fbd7c15
root <leoturorellana@gmail.com>
2014-11-08 18:32:40 -0430

    Hola otra vez

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
       index: <a href="rev2/index.php">version rev2 Hola mundo</a>
     </p>
     <p>
-      <a href="mysql.php">mysql</a>
+      <a href="mysql.php">mysql HOLA OTRO MUNDO</a>
     </p>
   </div>
 </body>
